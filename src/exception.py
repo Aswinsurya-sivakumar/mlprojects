b9d031276ddf3ef80927715f4445c2a8adb72e58
Aswin Surya <aswinsuryasivakumar2004@gmail.com>
2026-07-01 03:17:45 +0530

    EDA and Problems Statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detial(error,error_detial:sys):
     _,_,exc_tb = error_detial.exc_info()
@@ -7,7 +7,7 @@ def error_message_detial(error,error_detial:sys):
     error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name,exc_tb.tb_lineno,str(error) )
     return error_message
-    
+
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
@@ -16,3 +16,9 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+    if __name__ == "__main__":
+        try:
+            a=1/10
+        except Exception as e:
+            logging.info("Divide by Zero")
+            raise CustomException(e,sys)
